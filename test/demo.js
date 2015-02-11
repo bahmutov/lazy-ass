249d0db5f72a30f0500113de6e647cd33522b781
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-11 11:13:58 -0500

    using bad-line plugin for cute-stack

--- test/demo.js
+++ test/demo.js
@@ -1,37 +1,6 @@
 var cute = require('cute-stack');
-
-
-function myPlugin(frame) {
-  var exists = require('fs').existsSync;
-  var read = require('fs').readFileSync;
-
-  function getLine(filename, line) {
-    var source = read(filename, 'utf-8').split('\n');
-    return source[line - 1];
-  }
-  function isRelative(frame) {
-    var startsWithDot = /^\./;
-    return frame.file &&
-      startsWithDot.test(frame.file) &&
-      typeof frame.line === 'number' &&
-      exists(frame.file);
-  }
-  if (isRelative(frame)) {
-    console.log('relative frame');
-    console.log(frame);
-    var line = getLine(frame.file, frame.line);
-    if (line) {
-      frame.name = line.trim() + '\n  in ' + frame.id();
-    }
-    return cute.ui.default(frame);
-  } else {
-    return cute.ui.default(frame);
-  }
-}
-
-cute.ui.myPlugin = myPlugin;
-
-cute('myPlugin');
+cute.ui.badLine = require('bad-line');
+cute('badLine');
 
 require('..');
 /* global lazyAss */
