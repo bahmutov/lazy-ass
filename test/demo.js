1038df27de01eeae6b6b6d82d822d777a5f3cf09
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-02-11 10:43:07 -0500

    cute-stack plugin with printing offending line

--- test/demo.js
+++ test/demo.js
@@ -1,5 +1,41 @@
-require('cute-stack')();
+var cute = require('cute-stack');
+
+
+function myPlugin(frame) {
+  var exists = require('fs').existsSync;
+  var read = require('fs').readFileSync;
+
+  function getLine(filename, line) {
+    var source = read(filename, 'utf-8').split('\n');
+    return source[line - 1];
+  }
+  function isRelative(frame) {
+    var startsWithDot = /^\./;
+    return frame.file &&
+      startsWithDot.test(frame.file) &&
+      typeof frame.line === 'number' &&
+      exists(frame.file);
+  }
+  if (isRelative(frame)) {
+    console.log('relative frame');
+    console.log(frame);
+    var line = getLine(frame.file, frame.line);
+    if (line) {
+      frame.name = line.trim() + '\n  in ' + frame.id();
+    }
+    return cute.ui.default(frame);
+  } else {
+    return cute.ui.default(frame);
+  }
+}
+
+cute.ui.myPlugin = myPlugin;
+
+cute('myPlugin');
+
 require('..');
 /* global lazyAss */
-var foo = 'foo', bar = 'bar';
-lazyAss(false, 'this fails, foo =', foo, 'bar =', bar);
+function bad(foo, bar) {
+  lazyAss(false, 'this fails, foo =', foo, 'bar =', bar);
+}
+bad('foo', 'bar');
