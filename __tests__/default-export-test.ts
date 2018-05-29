84623a701f1bb4b8accc61193784a878d81b3f1f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-05-29 16:46:38 -0400

    chore: test default export

---
+++ __tests__/default-export-test.ts
@@ -0,0 +1,17 @@
+import la from '../src';
+
+describe('lazy-ass as default export', () => {
+  it('does not throw if condition is true', () => {
+    la(true)
+  })
+
+  it('does not throw when there is a message', () => {
+    la(true, 'text', 42, 'more text')
+  })
+
+  it('throws nice message', () => {
+    expect(() => {
+      la(false, 'text', 42, 'more text')
+    }).toThrowErrorMatchingSnapshot()
+  })
+})
